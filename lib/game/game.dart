52069594d66854a5a8f0a021819f99aecdf84b2d
Armel Soro <armel@rm3l.org>
2020-10-16 00:23:05 +0200

    Pave the way to retrieving data from a remote service
    Use a FutureBuilder with AsyncSnapshot for that purpose

--- lib/game/game.dart
+++ lib/game/game.dart
@@ -21,99 +21,164 @@ class Game extends StatefulWidget {
 
 class _GameState extends State<Game> {
   PageController _cardsController;
-  List<EssentielCardData> _allCardsData;
   int _currentPageIndex = 0;
+  int _numberOfCards = 0;
 
   @override
   void initState() {
     super.initState();
 
-    final allCategories = Category.values.toList();
-
-    _allCardsData =
-        allCategories.expand((category) => category.essentielCards()).toList();
-    _allCardsData.shuffle();
-
-    _cardsController = PageController(initialPage: _allCardsData.length);
+    _cardsController = PageController(initialPage: 300);
 
     ShakeDetector.autoStart(onPhoneShake: () {
       _randomDraw();
     });
   }
 
+  Future<List<EssentielCardData>> _getCardsDataFuture() {
+    //TODO Leverage GSheets API
+    final allCardsData = Category.values
+        .toList()
+        .expand((category) => category.essentielCards())
+        .toList();
+    allCardsData.shuffle();
+    return Future.delayed(Duration(seconds: 5), () => allCardsData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-    final screenHeight = MediaQuery.of(context).size.height;
-
-    return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(child: AnimatedBackground()),
-          _onBottom(AnimatedWave(
-            height: 180,
-            speed: 1.0,
-          )),
-          _onBottom(AnimatedWave(
-            height: 120,
-            speed: 0.9,
-            offset: pi,
-          )),
-          _onBottom(AnimatedWave(
-            height: 220,
-            speed: 1.2,
-            offset: pi / 2,
-          )),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Padding(
-              padding: EdgeInsets.only(
-                  top: screenHeight * 0.08, left: screenWidth * 0.03),
-              child: Text(
-                widget.title,
-                style: TextStyle(fontSize: 30.0, color: Colors.white),
-              ),
-            ),
-          ),
-          Positioned.fill(
-            child: Align(
-                alignment: Alignment.center,
-                child: Container(
-                  padding: EdgeInsets.only(
-                      left: screenWidth * 0.05, right: screenWidth * 0.05),
-                  height: screenHeight * 0.5,
-                  child: StackedCardCarousel(
-                    pageController: _cardsController,
-                    onPageChanged: (int pageIndex) {
-                      _currentPageIndex = pageIndex;
-                    },
-                    type: StackedCardCarouselType.cardsStack,
-                    items: _allCardsData.asMap().entries.map((entry) {
-                      final index = entry.key;
-                      final cardData = entry.value;
-                      return EssentialCard(
-                          cardData: cardData, onFlip: () => _jumpTo(index));
-                    }).toList(),
+    return FutureBuilder(
+        future: _getCardsDataFuture(),
+        builder: (BuildContext context,
+            AsyncSnapshot<List<EssentielCardData>> snapshot) {
+          Widget body;
+          if (snapshot.hasData) {
+            final allCardsData = snapshot.data;
+            if (allCardsData == null || allCardsData.isEmpty) {
+              _numberOfCards = 0;
+              body = Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                    Icon(
+                      Icons.error,
+                      color: Colors.red,
+                      size: 50,
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    Flexible(
+                        child: Text(
+                            "Pas de données trouvées pour initialiser le jeu. Merci de réessayer dans quelques instants.",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                                fontSize: 20,
+                                height: 1.7,
+                                color: Colors.white))),
+                  ]));
+            } else {
+              _numberOfCards = allCardsData.length;
+              body = StackedCardCarousel(
+                pageController: _cardsController,
+                onPageChanged: (int pageIndex) {
+                  _currentPageIndex = pageIndex;
+                },
+                type: StackedCardCarouselType.cardsStack,
+                items: allCardsData.asMap().entries.map((entry) {
+                  final index = entry.key;
+                  final cardData = entry.value;
+                  return EssentialCard(
+                      cardData: cardData, onFlip: () => _jumpTo(index));
+                }).toList(),
+              );
+            }
+          } else {
+            _numberOfCards = 0;
+            //Loading
+            body = Center(
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                  CircularProgressIndicator(),
+                  SizedBox(
+                    height: 20.0,
                   ),
+                  Flexible(
+                      child: Text(
+                          "Initialisation en cours. Merci de patienter quelques instants...",
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 20, height: 1.7, color: Colors.white))),
+                ]));
+          }
+
+          final screenWidth = MediaQuery.of(context).size.width;
+          final screenHeight = MediaQuery.of(context).size.height;
+
+          return Scaffold(
+            body: Stack(
+              children: [
+                Positioned.fill(child: AnimatedBackground()),
+                _onBottom(AnimatedWave(
+                  height: 180,
+                  speed: 1.0,
                 )),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () {
-          _randomDraw();
-        },
-        label: Text("Choisir une carte au hasard"),
-        icon: FaIcon(FontAwesomeIcons.random),
-        backgroundColor: Category.EVANGELISATION.color(),
-      ),
-    );
+                _onBottom(AnimatedWave(
+                  height: 120,
+                  speed: 0.9,
+                  offset: pi,
+                )),
+                _onBottom(AnimatedWave(
+                  height: 220,
+                  speed: 1.2,
+                  offset: pi / 2,
+                )),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(
+                        top: screenHeight * 0.08, left: screenWidth * 0.03),
+                    child: Text(
+                      widget.title,
+                      style: TextStyle(fontSize: 30.0, color: Colors.white),
+                    ),
+                  ),
+                ),
+                Positioned.fill(
+                  child: Align(
+                      alignment: Alignment.center,
+                      child: Container(
+                        padding: EdgeInsets.only(
+                            left: screenWidth * 0.05,
+                            right: screenWidth * 0.05),
+                        height: screenHeight * 0.5,
+                        child: body,
+                      )),
+                ),
+              ],
+            ),
+            floatingActionButton: (snapshot.hasData != null &&
+                    snapshot.data != null &&
+                    snapshot.data.isNotEmpty)
+                ? FloatingActionButton.extended(
+                    onPressed: () {
+                      _randomDraw();
+                    },
+                    label: Text("Je choisis une carte au hasard"),
+                    icon: FaIcon(FontAwesomeIcons.random),
+                    backgroundColor: Category.EVANGELISATION.color(),
+                  )
+                : null,
+          );
+        });
   }
 
   void _randomDraw() {
     final randomPick = RandomUtils.getRandomValueInRangeButExcludingValue(
-        0, _allCardsData.length, _currentPageIndex);
-    debugPrint("_currentPageIndex=$_currentPageIndex / randomPick=$randomPick");
+        0, _numberOfCards, _currentPageIndex);
+    debugPrint(
+        "_currentPageIndex=$_currentPageIndex / randomPick=$randomPick / _numberOfCards=$_numberOfCards");
     _jumpTo(randomPick);
   }
 

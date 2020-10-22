66b2b9a8268b1d228ce5265bd6f9c746b39b0e94
Armel Soro <armel@rm3l.org>
2020-10-22 23:56:15 +0200

    Remove Debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Essentiel',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //

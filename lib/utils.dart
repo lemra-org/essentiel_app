a0a067cf40ee8d68f7b24c47a910734268a7fbbd
Armel Soro <armel@rm3l.org>
2020-10-29 00:41:12 +0100

    Display Showcase widget for scrolling card list

--- lib/utils.dart
+++ lib/utils.dart
@@ -1,5 +1,8 @@
 import 'dart:math';
 
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 class RandomUtils {
   static final _random = Random();
   static final Map<dynamic, int> _cache = Map();
@@ -27,4 +30,24 @@ class RandomUtils {
 
   static int getNamedRandomValueInRange<T>(T key, int min, int max) =>
       _cache.putIfAbsent(key, () => getRandomValueInRange(min, max));
+}
+
+class AppUtils {
+  static const PREFERENCES_IS_FIRST_LAUNCH_STRING = "isFirstLaunch";
+
+  static Future<bool> isFirstLaunch() async {
+    final sharedPreferences = await SharedPreferences.getInstance();
+    final bool isFirstLaunch = sharedPreferences
+            .getBool(AppUtils.PREFERENCES_IS_FIRST_LAUNCH_STRING) ??
+        true;
+
+    debugPrint("isFirstLaunch: $isFirstLaunch");
+
+    if (isFirstLaunch) {
+      sharedPreferences.setBool(
+          AppUtils.PREFERENCES_IS_FIRST_LAUNCH_STRING, false);
+    }
+
+    return isFirstLaunch;
+  }
 }
